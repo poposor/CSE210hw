14390e07ff1613284a922d7219630967b43b2bb1
Caden Reeder <cadenreeder@gmail.com>
2025-09-18 13:19:31 -0600

    Finish Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string lastName;
+        string firstName;
+
+        Console.Write("What is your first name?");
+        firstName = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
