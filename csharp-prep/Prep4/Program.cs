357a0377e6e32b37431f6c2a12242ebb5a1d39f5
Caden Small Laptop <cadenreeder@gmail.com>
2025-09-23 13:36:50 -0600

    Finished Prep4 number list project

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> yourList = new List<int>();
+        int last = -1;
+        do
+        {
+            Console.Write("Enter number: ");
+            last = int.Parse(Console.ReadLine());
+            if (last != 0)
+            {
+                yourList.Add(last);
+            }
+        } while (last != 0);
+        int sum = 0;
+        int largest = 0;
+        foreach (int i in yourList)
+        {
+            sum += i;
+            largest = Math.Max(largest, i);
+        }
+        Console.WriteLine("sum: " + sum);
+        Console.WriteLine("average: " + ((float)sum / yourList.Count));
+        Console.WriteLine("largest: " + largest);
+
     }
 }
