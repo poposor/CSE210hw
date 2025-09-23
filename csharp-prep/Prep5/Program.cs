15a4d9d644c4e3e41817dab3f1895d9c7a9dbb8f
Caden Small Laptop <cadenreeder@gmail.com>
2025-09-23 14:07:23 -0600

    Finish Prep5 function project

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name?: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number?: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        Console.Write("What is your birth year?: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+    static void DisplayResult(string name, int squared, int birthYear)
+    {
+        Console.WriteLine(name + ", your number squared is " + squared);
+        Console.WriteLine(name + ", your will turn " + (2025-birthYear) + " this year");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int birthYear;
+        PromptUserBirthYear(out birthYear);
+        int squareResult = SquareNumber(userNumber);
+        DisplayResult(userName, squareResult, birthYear);
+
     }
 }
