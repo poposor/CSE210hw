9701ea37592b6a51882fa3278594e3f4ad09fcec
Caden Small Laptop <cadenreeder@gmail.com>
2025-09-23 13:18:47 -0600

    Completed Prep3 random number guesser

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        String keepPlaying = "yes";
+        while (keepPlaying == "yes")
+        {
+            Random randomGenerator = new Random();
+            int goal = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guesses = -1;
+            while (guess != goal)
+            {
+                guesses += 1;
+                Console.WriteLine("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < goal)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (guess > goal)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine("Correct!");
+                }
+            }
+            Console.WriteLine("It took you " + guesses + " tries. Type 'yes' to play again");
+            keepPlaying = Console.ReadLine();
+        }
     }
 }
