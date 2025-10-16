3f330382fdf911129685724268c9f6684caeb770
Caden Small Laptop <cadenreeder@gmail.com>
2025-10-16 11:01:58 -0600

    encapsulation learning completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac = new Fraction();
+        Fraction frac1 = new Fraction(5);
+        Fraction frac2 = new Fraction(3, 4);
+        Fraction frac3 = new Fraction(1, 3);
+
+        Console.WriteLine(frac.GetFractionString());
+        Console.WriteLine(frac.GetBottom());
+
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetTop());
+
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
     }
 }
