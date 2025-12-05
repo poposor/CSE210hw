6e876c559a378a5ce4f77dd3da21a56807b558b1
Caden Reeder <cadenreeder@gmail.com>
2025-12-04 17:31:37 -0700

    Deleted foundation files

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,29 @@
 using System;
-
+using System.Net.Http;
+using System.Threading.Tasks;
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main()
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // string fileUrl = "https://byui.instructure.com/feeds/calendars/user_MsN9hQyH8YwVHMQaYZlbkVdNPZbBk7aAjhOKLGjy.ics"; // Replace with your URL
+
+        // using (HttpClient client = new HttpClient())
+        // {
+        //     try
+        //     {
+        //         // Download the file content as a string
+        //         string fileContents = await client.GetStringAsync(fileUrl);
+                
+        //         Console.WriteLine("File contents:");
+        //         Console.WriteLine(fileContents);
+        //     }
+        //     catch (HttpRequestException e)
+        //     {
+        //         Console.WriteLine($"Error downloading file: {e.Message}");
+        //     }
+        // }
+        string dateString = "12/15/2023 10:30:00 AM";
+        DateTime resultDate = DateTime.Parse(dateString);
+        Console.WriteLine(resultDate); 
     }
 }
