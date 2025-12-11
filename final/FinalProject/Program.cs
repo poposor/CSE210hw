28441470e3e05feca5ae215a72adceea4062f56b
poposor <cadenreeder@gmail.com>
2025-12-11 13:41:38 -0700

    Comment update

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -2,7 +2,7 @@
 // Features:
 // Create new event
 // Delete event (Click the delete button and then click the event you wish to delete)
-// Load txt into the calender
+// Load txt into the calender (type "canvas.txt") for my example calender events
 // Save calender into txt
 // Get calender from canvas (On the canvas calender page in the bottom right corner there is a link titiled "Calendar Feed" clicking it will show you a link you can copy into the program)
 // Origianly I was planning on adding Goals to this project and I did implement the Goal class but decided against adding them to the final interface
@@ -17,10 +17,7 @@ static class Program
     async static Task Main()
     {
         Calender myCalendar = new Calender();
-
-        // Uncomment the line bellow if you don't feel like getting your own calender
-        // myCalendar.Load("canvas.txt");
-
+        
         Window app = new Window(myCalendar);
         Application.Run(app);
 
